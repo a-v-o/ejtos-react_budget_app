76636b05f4943886793259cf80165d5e93784a08
Funmito <adejuwonvictor2004@gmail.com>
2023-12-17 18:37:54 -0500

    Made improvements

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, currency } = useContext(AppContext);
     const { expenses } = useContext(AppContext);
     const totalExpenses = expenses.reduce((total, item) => {
         return (total += item.cost);
@@ -19,8 +19,8 @@ const Budget = () => {
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+<span>Budget: {currency}</span>
+<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}>{budget}</input>
 </div>
     );
 };
--- src/components/Currency.js
+++ src/components/Currency.js
@@ -4,24 +4,26 @@ import { AppContext } from '../context/AppContext';
 export default function Currency() {
     const {dispatch, currency} = useContext(AppContext)
     let newCurrency = currency
+    let currencyName = 'Pound'
 
     function changeCurrency(e) {
 
         newCurrency = e.target.value
+        currencyName = newCurrency.substr(1)
 
         dispatch({
             type:'CHG_CURRENCY',
-            payload: newCurrency[0]
+            payload: newCurrency.substr(0, 1)
         })
     }
 
     return <div>
-        <select name="currency" id="inputGroupSelect03" className='form-select' onChange={changeCurrency}>
-            <option selected>Currency ({newCurrency} Pound)</option>
-            <option value="Pound">{'£ Pound'}</option>
-            <option value="Dollar">{'$ Dollar'}</option>
-            <option value="Euro">{'€ Euro'}</option>
-            <option value="Ruppee">{'₹ Ruppee'}</option>
+        <select name="currency" id="inputGroupSelect03" className='form-select' style={{backgroundColor:'#3EB489'}} onChange={changeCurrency}>
+            <option disabled selected>Currency ({newCurrency} {currencyName})</option>
+            <option value="£ Pound">£ Pound</option>
+            <option value="$ Dollar">$ Dollar</option>
+            <option value="€ Euro">€ Euro</option>
+            <option value="₹ Ruppee">₹ Ruppee</option>
         </select>
     </div>
 
